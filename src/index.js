fdfdcb1bfc829708c645555c4324052ccdc21114
rafikize <140318598+rafikize@users.noreply.github.com>
2024-06-07 16:21:23 +0200

    fin de projet Blog

--- src/index.js
+++ src/index.js
@@ -1,2 +1,55 @@
 import "./assets/styles/styles.scss";
 import "./index.scss";
+
+const articleContainerElement = document.querySelector(".articles-container");
+const createArticles = (articles) => {
+  const articlesDom = articles.map((article) => {
+    const articleDom = document.createElement("div");
+    articleDom.classList.add("article");
+    articleDom.innerHTML = `
+                    <img src="${article.img}" alt="profil" />
+                    <h2>${article.title}</h2>
+                    <p class="article-author">${article.author} - ${article.category}</p>
+                    <p class="article-content">
+                        ${article.content}
+                    </p>
+                    <div class="article-action">
+                        <button class="btn btn-danger" data-id=${article._id}>Supprimer</button>
+                    </div> 
+    `;
+    return articleDom;
+  });
+  articleContainerElement.innerHTML = "";
+  articleContainerElement.append(...articlesDom);
+  const deleteButtons = articleContainerElement.querySelectorAll(".btn-danger");
+    deleteButtons.forEach((button) => {
+        button.addEventListener("click", async (event) => {
+        const id = event.target.getAttribute("data-id");
+        try {
+            const response = await fetch(`https://restapi.fr/api/article/${id}`, {
+            method: "DELETE",
+            });
+            const body = await response.json();
+            console.log(body);
+            fetchArticle();
+        } catch (e) {
+            console.error("e : ", e);
+        }
+        });
+    });
+};
+
+const fetchArticle = async () => {
+  try {
+    const response = await fetch("https://restapi.fr/api/article");
+    let articles = await response.json();
+    if (!Array.isArray(articles)) {
+      articles = [articles];
+    }
+    createArticles(articles);
+  } catch (error) {
+    console.log("error");
+  }
+};
+
+fetchArticle();
